4d8cbb783a03301b7dfa3b1b0d98d166ccfae212
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-29 22:00:26 +0300

    add sign in with google

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,13 +2,41 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function Component() {
+  const [user, setUser] = useState<any>([]);
+  const [profile, setProfile] = useState<any>([]);
+
+  const loginWithGoogle = useGoogleLogin({
+    onSuccess: (response: any) => {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${response.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${response.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          toast.success(`${res.data.name}`);
+        })
+        .catch((err) => console.log(err));
+    },
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-        <div className="hidden bg-muted lg:block ">
+      <div className="hidden bg-muted lg:block ">
         <img
-      src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Image"
           width="1920"
           height="1080"
@@ -19,26 +47,46 @@ export default function Component() {
         <div className="mx-auto grid w-[350px] lg:w-[500px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">Enter your email below to login to your account</p>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" required className="w-full" />
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                className="w-full"
+              />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
-                <Link to="/forgot_password" className="ml-auto inline-block text-sm underline" >
+                <Link
+                  to="/forgot_password"
+                  className="ml-auto inline-block text-sm underline"
+                >
                   Forgot your password?
                 </Link>
               </div>
-              <Input id="password" type="password" required className="w-full" />
+              <Input
+                id="password"
+                type="password"
+                required
+                className="w-full"
+              />
             </div>
             <Button type="submit" className="w-full">
               Login
             </Button>
-            <Button variant="outline" className="w-full">
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={loginWithGoogle}
+            >
               Login with Google
             </Button>
             <Button variant="outline" className="w-full">
@@ -50,13 +98,12 @@ export default function Component() {
           </div>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link to="#" className="underline" >
+            <Link to="#" className="underline">
               Sign up
             </Link>
           </div>
         </div>
       </div>
-    
     </div>
   );
 }
