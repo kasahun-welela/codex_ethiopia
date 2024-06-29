64bd51e89ca0a5a029749318f2b7b68053bdb386
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-29 22:00:06 +0300

    install toastify

--- src/main.tsx
+++ src/main.tsx
@@ -16,10 +16,13 @@ import Blog from "./pages/Blog.tsx";
 import BlogList from "./components/BlogList.tsx";
 import Contact from "./pages/contact.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId="370357882485-ruiednd7plrsmn91kjp4vdcp53ivmuvc.apps.googleusercontent.com">
     <React.StrictMode>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
