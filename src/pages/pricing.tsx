34fae51249937c8cb11687073da55d735a9a37b6
Abenezer <abenezerkifle000@gmail.com>
2024-06-29 01:19:45 -0700

    Remove the header

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,13 +1,13 @@
-import Hero from "@/components/Hero";
+// import Hero from "@/components/Hero";
 import Price from "@/components/Pricing";
 
 const pricing = () => {
   return (
     <>
-      <Hero
+      {/* <Hero
         title="Pricing"
         description="Choose the plan that fits your needs. All plans are billed monthly and exclude VAT"
-      />
+      /> */}
       <Price />
     </>
   );
