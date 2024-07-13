54a9ce6ae3ba5c588da6ef8e48e1f97cf3619d36
Abenezer <abenezerkifle000@gmail.com>
2024-07-13 02:02:46 -0700

    Import and user terms and condition componet

---
+++ src/pages/TermsConditions.tsx
@@ -0,0 +1,11 @@
+import TermsAndConditions from "@/components/TermsConditions"
+
+const TermsConditions = () => {
+  return (
+    <div>
+        <TermsAndConditions/>
+    </div>
+  )
+}
+
+export default TermsConditions
