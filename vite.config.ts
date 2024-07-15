cba6b237f2a2a1caafd0418775511e42b06cc1e1
Abenezer Kifle <52881874+abenooo@users.noreply.github.com>
2024-07-15 01:37:04 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,27 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
+    build: {
+      outDir: 'build',  // Specifies the output directory for build files
+    }
   };
 });
+
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig, loadEnv } from "vite";
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//   return {
+//     define: {
+//       "process.env": env,
+//     },
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "./src"),
+//       },
+//     },
+//   };
+// });
