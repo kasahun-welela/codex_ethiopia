68ea3fb40747b79ff1972c8276850e29c18c8cc2
Abenezer <abenezerkifle000@gmail.com>
2024-07-17 20:46:37 -0700

    Update the config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
+    base: '/public_html/', // This sets the base path for your assets
     define: {
       "process.env": env,
     },
@@ -15,15 +16,45 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
+      outDir: 'public_html', // This sets the output directory for your build
       rollupOptions: {
         output: {
           manualChunks: {
-            // Customize chunking strategy, for example:
             vendor: ['react', 'react-dom'],
           },
         },
       },
-      chunkSizeWarningLimit: 2000,  // Adjust the warning limit as needed
+      chunkSizeWarningLimit: 2000,
     },
   };
 });
+
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig, loadEnv } from "vite";
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//   return {
+//     define: {
+//       "process.env": env,
+//     },
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "./src"),
+//       },
+//     },
+//     build: {
+//       rollupOptions: {
+//         output: {
+//           manualChunks: {
+//             // Customize chunking strategy, for example:
+//             vendor: ['react', 'react-dom'],
+//           },
+//         },
+//       },
+//       chunkSizeWarningLimit: 2000,  // Adjust the warning limit as needed
+//     },
+//   };
+// });
